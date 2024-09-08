d2bbec3ff68ffc343310f1d24b980be3dabda24b
Lain4504 <tienhuynhn2004@gmail.com>
2024-09-08 12:32:58 +0700

    revert

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/ReactToastify.css'
+const App = () => {
+  return(
+    <div>
+    <ToastContainer/>
+    <Navbar/>
+    <Routes>
+        <Route path='/' element={<Home />} />
+    </Routes>
+    <Footer/>
+   </div>
   )
 }
+export default App;
