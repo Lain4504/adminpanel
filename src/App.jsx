0ca53593f7d0b2868b0e3fb87eed0de452cce838
Lain4504 <tienhuynhn2004@gmail.com>
2024-09-10 16:53:06 +0700

    create sidebar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import React from "react"
 import { Routes, Route } from "react-router-dom"
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/ReactToastify.css'
+import Home from "./pages/Home";
+import Sidebar from "./components/Sidebar";
+
 const App = () => {
   return(
     <div>
     <ToastContainer/>
-    <Navbar/>
+    <Sidebar/>
     <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home/>} />
     </Routes>
-    <Footer/>
    </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+   
+    <footer className="mt-auto">
+      <hr />
+      <p className="py-5 text-sm text-center bg-gray-800 text-white">
+        Copyright 2024 @ Book Store - All Rights Reserved
+      </p>
+    </footer>
+  </div>
+  )
+}
+
+export default Footer
