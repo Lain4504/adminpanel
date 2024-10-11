c0b5e285dbacbd1dfa6dca8309946b4354cc54b1
Lain4504 <tienhuynhn2004@gmail.com>
2024-10-12 00:43:16 +0700

    Create Post Update

--- src/components/Page404.jsx
+++ src/components/Page404.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Page404 = () => {
     return (
       <>
-        <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
           <div className="text-center">
             <p className="text-4xl font-semibold text-indigo-600">404</p>
             <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Không tìm thấy trang</h1>
